f111315c9cca81dd636e26b71a2356cc35faf1e9
alerjr <110341419+alerjr@users.noreply.github.com>
2023-06-10 17:26:07 -0300

    Add files via upload

---
+++ pages/createaccount/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cadastros';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+?>
